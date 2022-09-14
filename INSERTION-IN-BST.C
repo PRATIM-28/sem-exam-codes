2220f2910aa73ece2880f3d25652768a97577876
PARTHA PRATIM KHAMARI <109213675+PRATIM-28@users.noreply.github.com>
2022-09-14 17:30:49 +0530

    Create INSERTION-IN-BST.C

---
+++ INSERTION-IN-BST.C
@@ -0,0 +1,111 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node* left;
+    struct node* right;
+};
+
+struct node* newnode(int val)
+{
+    struct node* n=(struct node*)malloc(sizeof(struct node));
+    n->data=val;
+    n->left=NULL;
+    n->right=NULL;
+    return n;
+}
+
+void inorder(struct node* root)
+{
+    if(root!=NULL)
+    {
+        inorder(root->left);
+        printf("%d ",root->data);
+        inorder(root->right);
+    }
+}
+
+int isBST(struct node* root)
+{
+    static struct node* prev=NULL;
+    if(root!=NULL)
+    {
+        if(!isBST(root->left))
+        {
+            return 0;
+        }
+        if(prev!=NULL&&root->data<=prev->data)
+        {
+            return 0;
+        }
+        prev=root;
+        return isBST(root->right);
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+void insert(struct node* root,int key)
+{
+    struct node* prev=NULL;
+    while(root!=NULL)
+    {
+        prev=root;
+        if(root->data==key)
+        {
+            printf("the key %d cannot be inserted",key);
+            return;
+        }
+        else if(root->data<key)
+        {
+            root=root->right;
+        }
+        else
+        {
+            root=root->left;
+        }
+    }
+    struct node*n=newnode(key);
+    if(prev->data<key)
+    {
+        prev->right=n;
+    }
+    else
+    {
+        prev->left=n;
+    }
+}
+
+
+int main()
+{
+    struct node* p=newnode(8);
+    struct node* p1=newnode(5);
+    struct node* p2=newnode(11);
+    struct node* p3=newnode(4);
+    struct node* p4=newnode(7);
+    struct node* p5=newnode(9);
+    struct node* p6=newnode(12);
+   // struct node* p7=newnode(7);
+    
+    p->left=p1;
+    p->right=p2;
+    p1->left=p3;
+    p1->right=p4;
+    p2->left=p5;
+    p2->right=p6;
+    
+    inorder(p);
+    printf("\n");
+    printf("%d",isBST(p));
+    
+    insert(p,10);
+    
+    printf("\n%d",p5->right->data);
+    printf("\n");
+    inorder(p);
+}
